4c52fe3cfe03c58a3bdf1630cdc8f1601a841e92
Juan Ortega <juanmanuelom1908@gmail.com>
2026-09-11 00:04:00 +0000

    Fix timestamp microservice: optional date param for Express 5

--- build-a-timestamp-microservice/server.js
+++ build-a-timestamp-microservice/server.js
@@ -15,6 +15,32 @@ app.get("/", (_req, res) => {
 
 // Do not change code below this line
 
+app.get("/api{/:date}",(req,res)=>{
+  const dateParam = req.params.date;
+  let dateObj;
+
+  if (!dateParam) {
+  dateObj = new Date();
+} else if (/^\d+$/.test(dateParam)) {
+  dateObj = new Date(parseInt(dateParam));
+} else {
+  dateObj = new Date(dateParam);
+}
+
+  if(isNaN(dateObj.getTime())){
+    return res.json({
+      error: "Invalid Date"
+    })
+  }
+
+  res.json({
+    unix: dateObj.getTime(),
+    utc: dateObj.toUTCString()
+  });
+})
+
+
+
 const PORT = 8000;
 const listener = app.listen(PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
